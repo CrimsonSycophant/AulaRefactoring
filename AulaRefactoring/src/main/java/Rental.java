33af650da732a4cc7a325edcbd630e1cee49f18d
CrimsonSycophant <54426860+CrimsonSycophant@users.noreply.github.com>
2019-11-11 12:05:22 -0300

    Refactoring 5

--- AulaRefactoring/src/main/java/Rental.java
+++ AulaRefactoring/src/main/java/Rental.java
@@ -40,5 +40,11 @@ class Rental {
            break;
      }
      return result;
-}
+    }
+    int getFrequentRenterPoints() {
+       if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+          return 2;
+       else
+          return 1;
+   }
 }
